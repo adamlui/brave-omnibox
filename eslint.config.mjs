911543937dd42be0ca049be2246177d78512250c
adamlui@protonmail.com <adamlui@protonmail.com>
2024-11-21 13:38:26 -0800

    Moved js-styles/no-trailing-spaces rule up for readability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,9 +12,9 @@ export default [
         rules: {
             ...js.configs.recommended.rules,
             'indent': 'off', 'no-unexpected-multiline': 'off', 'key-spacing': 'off', // allow whitespace anywhere
+            'js-styles/no-trailing-spaces': 'error', // ...except at ends of lines
             'quotes': ['error', 'single', { 'allowTemplateLiterals': true }], // enforce single quotes except backticks to avoid escaping quotes
             'comma-dangle': ['error', 'never'], // enforce no trailing commas in arrays or objects
-            'js-styles/no-trailing-spaces': 'error', // disallow whitespace at ends of lines
             'no-async-promise-executor': 'off', // allow promise executor functions to be async (to accomodate await lines)
             'no-constant-condition': 'off', // allow constant conditions
             'no-empty': 'off', // allow empty blocks
